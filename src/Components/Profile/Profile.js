f3f59689d8dbf00e282c4099134ec0bc6a9af1d4
Monks09 <mayanksharma5104@gmail.com>
2023-03-13 02:43:12 +0530

    aj ka last commit

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -3,15 +3,15 @@ import "./Profile.css";
 function Profile() {
     return (
         <div className="UserProfile" id="profile">
-            <div>
+            <div className="intro-div">
                 <h1>Hi, I am Mayank Sharma.</h1>
                 <p>I am a Full Stack Web Developer with knowledge in Frontend and Backend technologies and I make user-friendly websites.</p>
                 <button className="resume-button">
                     <a href="Mayank_Sharma_Resume.pdf" download>Download Resume</a>
                 </button>
             </div>
-            <div>
-                <img style={{ height: "250px", borderRadius: "50%", boxShadow: "0 0 10px black" }} src="my-photo-1.jpg" alt="avatar" />
+            <div className="avatar-div">
+                <img id="avatar" src="my-photo-1.jpg" alt="avatar" />
             </div>
 
         </div>
